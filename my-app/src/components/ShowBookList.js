e4ca44c875c271808bf8e01e0186a5fdb0636841
Toby <mcisaactoby@gmail.com>
2023-08-13 21:08:23 +1200

    changed links from localhost to the vercel url

--- my-app/src/components/ShowBookList.js
+++ my-app/src/components/ShowBookList.js
@@ -9,7 +9,7 @@ function ShowBookList() {
 
   useEffect(() => {
     axios
-      .get('http://localhost:8082/api/books')
+      .get('https://cise-week3-backend-dm1q.vercel.app/api/books')
       .then((res) => {
         setBooks(res.data);
       })
